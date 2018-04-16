f1b24e72727cc6e1de94cff4cd225bd654e02271
Vladimir <boooobka@gmail.com>
2018-04-16 21:58:42 +0400

    poins to cube

--- src/Group/Group.js
+++ src/Group/Group.js
@@ -9,84 +9,41 @@ class Group extends Component {
     componentWillMount() {
         
         this.group = new THREE.Group();;
+
         
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
 
-        for ( let i = 0; i < this.geometry.faces.length; i +=2 ) {
-            let color =  Math.random() * 0xffffff;
-            this.geometry.faces[ i ].color.setHex(color);
-            this.geometry.faces[ i + 1 ].color.setHex(color);
-        }
+        this.props.points.forEach((function(point){
 
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube1 = new THREE.Mesh(this.geometry, this.material);
-        this.cube1.position.set(0,1,0);
-
-        for ( let i = 0; i < this.geometry.faces.length; i +=2 ) {
-            let color =  Math.random() * 0xffffff;
-            this.geometry.faces[ i ].color.setHex(color);
-            this.geometry.faces[ i + 1 ].color.setHex(color);
-        }
+            this.geometry = new THREE.BoxGeometry(1, 1, 1);
+            for ( let i = 0; i < this.geometry.faces.length; i +=2 ) {
+                    let color =  Math.random() * 0xffffff;
+                    this.geometry.faces[ i ].color.setHex(color);
+                    this.geometry.faces[ i + 1 ].color.setHex(color);
+                }
+                this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
+                this.cube = new THREE.Mesh(this.geometry, this.material);
+                
 
 
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube2 = new THREE.Mesh(this.geometry, this.material);
-        this.cube2.position.set(1,1,0);
-        for ( let i = 0; i < this.geometry.faces.length; i +=2 ) {
-            let color =  Math.random() * 0xffffff;
-            this.geometry.faces[ i ].color.setHex(color);
-            this.geometry.faces[ i + 1 ].color.setHex(color);
-        }
+                this.cube.position.set(point.x,point.y,point.z);
+
+                
 
 
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube3 = new THREE.Mesh(this.geometry, this.material);
+                //let geo = new THREE.EdgesGeometry( this.cube ); // or WireframeGeometry
+                //let mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 4 } );
+              //  let wireframe = new THREE.LineSegments( geo, mat );
+               // this.cube.add( wireframe );
 
-        this.cube3.position.set(0,0,0);
+                this.group.add(this.cube);
 
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube4 = new THREE.Mesh(this.geometry, this.material);
+        }).bind(this));       
 
-        this.cube4.position.set(1,0,0);
+                
 
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube5 = new THREE.Mesh(this.geometry, this.material);
+        console.log(this.props.points);
 
-        this.cube5.position.set(-1,0,0);
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube6 = new THREE.Mesh(this.geometry, this.material);
-
-        this.cube6.position.set(-1,-1,0);
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube7 = new THREE.Mesh(this.geometry, this.material);
-
-        this.cube7.position.set(-1,1,0);
-
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube8= new THREE.Mesh(this.geometry, this.material);
-
-        this.cube8.position.set(0,-1,0);
-
-        this.material = new THREE.MeshBasicMaterial({ color: 0xFFFFF, vertexColors: THREE.FaceColors});
-        this.cube9= new THREE.Mesh(this.geometry, this.material);
-
-        this.cube9.position.set(1,-1,0);
-
-
-
-        this.group.add(this.cube1);
-        this.group.add(this.cube2);
-        this.group.add(this.cube3);
-        this.group.add(this.cube4);
-        this.group.add(this.cube5);
-        this.group.add(this.cube6);
-        this.group.add(this.cube7);
-        this.group.add(this.cube8);
-        this.group.add(this.cube9);
+      
         this.context.scene.add(this.group);
         
 
@@ -99,7 +56,7 @@ class Group extends Component {
 
     getCoords(){
         var position1 = new THREE.Vector3();
-        position1.getPositionFromMatrix( this.cube1.matrixWorld );
+        position1.setFromMatrixPosition( this.cube1.matrixWorld );
 
     }
 
@@ -109,19 +66,24 @@ class Group extends Component {
 
 
     componentDidUpdate() {
-        const { rotation, position } = this.props;
+        const { rotation } = this.props;
        // console.log(this.group.rotation)
-        this.group.rotation.z = this.group.rotation.z  <= Math.PI/2 ? this.group.rotation.z + 0.01 : this.group.rotation.z;
+
+        this.group.rotation.z = rotation.z
+
+
+
+        //this.group.rotation.z  <= Math.PI/2 ? this.group.rotation.z + 0.025 : this.group.rotation.z;
        // this.group.rotation.y +=0.01 ;
        // this.group.rotation.z = this.props.rotation.z;    
-       if(this.group.rotation.z ){
+      /* if(this.group.rotation.z ){
        var position1 = new THREE.Vector3();
-        position1.getPositionFromMatrix( this.cube1.matrixWorld );
-        console.log(position1.x + ',' + position1.y + ',' + position1.z);
-        position1.getPositionFromMatrix( this.cube2.matrixWorld );
-        console.log(2222)
-        console.log(position1.x + ',' + position1.y + ',' + position1.z);
-       }
+        position1.setFromMatrixPosition( this.cube1.matrixWorld );
+      //  console.log(position1.x + ',' + position1.y + ',' + position1.z);
+        position1.setFromMatrixPosition( this.cube2.matrixWorld );
+      //  console.log(2222)
+      //  console.log(position1.x + ',' + position1.y + ',' + position1.z);
+       }*/
       
     }
 
